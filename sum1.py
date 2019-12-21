85f9dcf90bf6a8368ae4bc2418b7753b4eaf8892
nlakshmanan <nnlnachu@gmail.com>
2019-12-20 20:09:49 -0700

    Misc changes

--- sum1.py
+++ sum1.py
@@ -1,14 +1,15 @@
 print("new file")
 print("I am a sum file")
-<<<<<<< HEAD
+
 def addition_extra(a,b):
 	return a+b+1
 def div_extra(a,b,c):
 	return a/b
-def mod(a%b):
+def mod(a,b):
 	return a%b
-<<<<<<< Updated upstream
+
 def addition(a,b):
-	return a+b5
-def sub(a-b-c):
-	retun sub
+	return a+b
+
+def sub(a,b,c):
+	return a-b-c
